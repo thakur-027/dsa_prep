ba204c8c329b2117175511d55da5b522adf097ef
Ayush Thakur <ayushtkr.23@gmail.com>
2026-03-12 17:48:56 +0530

    daily temp

---
+++ stacks_and_queues/daily_temperatures.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> s;
+
+        for(int i = 0; i < n; i++) {
+
+            while(!s.empty() && temperatures[i] > temperatures[s.top()]) {
+                
+                int prev = s.top();
+                s.pop();
+
+                ans[prev] = i - prev;
+            }
+
+            s.push(i);
+        }
+
+        return ans;
+    }
+};
