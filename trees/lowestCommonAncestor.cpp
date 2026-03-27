cd63d6164ef573a14d20611ffeab35d222841069
Ayush Thakur <ayushtkr.23@gmail.com>
2026-03-27 10:20:02 +0530

    Clean up commented code in lowestCommonAncestor.cpp
    Removed commented-out code for TreeNode structure.

--- trees/lowestCommonAncestor.cpp
+++ trees/lowestCommonAncestor.cpp
@@ -5,8 +5,7 @@
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+ * };*/
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
