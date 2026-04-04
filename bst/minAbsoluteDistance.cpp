d3751d3c5c87fa0b8b234d4a1021cc5faa7ab5e5
Ayush Thakur <ayushtkr.23@gmail.com>
2026-04-04 14:50:20 +0530

    difference

---
+++ bst/minAbsoluteDistance.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* prev = NULL;
+    int getMinimumDifference(TreeNode* root) {    
+
+        if(root == NULL)
+        return INT_MAX;
+
+        int ans = INT_MAX;
+        if(root -> left){
+            int leftMin = getMinimumDifference(root -> left);
+            ans = min(ans, leftMin);
+        }
+
+        if(prev != NULL)
+        ans = min(ans, root->val - prev->val);
+        prev = root;
+
+        if(root -> right){
+            int rightMin = getMinimumDifference(root -> right);
+            ans = min(ans, rightMin);
+        }
+        return ans;
+    }
+};
