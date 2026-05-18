560381c99c0ef650602136552541e433b5e60527
Ayush Thakur <ayushtkr.23@gmail.com>
2026-05-18 08:27:22 +0530

    Add initial merge logic for sorted arrays

--- array/merge_sorted_array.cpp
+++ array/merge_sorted_array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
         int j = 0;
 
         for(int i = 0; i < nums1.size() && j < n; i++) {
