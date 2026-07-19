c978a21e60f4136315ac7fb226155b5392e25f8c
Ayush Thakur <ayushtkr.23@gmail.com>
2026-07-19 08:36:40 +0530

    Create maxProductSubarray.cpp

---
+++ array/maxProductSubarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxProduct = nums[0];
+        int minProduct = nums[0];
+        int answer = nums[0];
+
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] < 0){
+            swap(maxProduct, minProduct);
+            }
+            maxProduct = max(nums[i], maxProduct*nums[i]);
+            minProduct = min(nums[i], minProduct*nums[i]);
+            answer = max(answer, maxProduct);
+        }
+        return answer;
+    }
+};
