a6991493f781e28324402412932e40e6e5484bdc
Ayush Thakur <ayushtkr.23@gmail.com>
2026-07-20 08:02:11 +0530

    Update group_anagrams.cpp

--- arr_strs/group_anagrams.cpp
+++ arr_strs/group_anagrams.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
         unordered_map<string, vector<string>> mp;
 
         for (int i = 0; i < strs.size(); i++) {
