a9239ca10d6a65cbd745f468f23ff7dac91b8ff5
Ayush Thakur <ayushtkr.23@gmail.com>
2026-07-27 06:58:10 +0530

    Update linked_list1.cpp

--- linked_list/linked_list1.cpp
+++ linked_list/linked_list1.cpp
@@ -11,7 +11,6 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
-
         while(fast != NULL && fast->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
