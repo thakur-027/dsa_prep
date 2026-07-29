6650515ad43f551ad541665e5b5e543c4d3d20e2
Ayush Thakur <ayushtkr.23@gmail.com>
2026-07-29 08:52:06 +0530

    Update validParentheses.cpp

--- stacks_and_queues/validParentheses.cpp
+++ stacks_and_queues/validParentheses.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isValid(string str) {
         stack<char> st;
-
         for(int i=0; i<str.size(); i++){
             if(str[i] == '(' || str[i] == '{' || str[i] == '['){
                 st.push(str[i]);
