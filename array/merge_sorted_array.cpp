d7cc1cd13e90cbe9bfb0c81f1654039c30d3a3ee
Ayush Thakur <ayushtkr.23@gmail.com>
2026-08-08 05:45:32 +0530

    Update merge_sorted_array.cpp

--- array/merge_sorted_array.cpp
+++ array/merge_sorted_array.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        int j = 0;
+       int i = m - 1;
+       int j = n - 1;
+       int k = m + n - 1;
 
-        for(int i = 0; i < nums1.size() && j < n; i++) {
-            if(nums1[i] == 0) {
-                nums1[i] = nums2[j];
-                j++;
-            }
+       while(j >= 0){
+        if(i >= 0 && nums1[i] > nums2[j]){
+            nums1[k--] = nums1[i--];
         }
-
-        sort(nums1.begin(), nums1.end());
+        else{
+            nums1[k--] = nums2[j--];
+        }
+       }
     }
 };
-
