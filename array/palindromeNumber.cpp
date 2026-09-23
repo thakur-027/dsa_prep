1dc1c1416eff2a95e7643e6b181a57f1783ece39
Ayush Thakur <ayushtkr.23@gmail.com>
2026-09-23 05:32:04 +0530

    Implement palindrome check for an integer

---
+++ array/palindromeNumber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
